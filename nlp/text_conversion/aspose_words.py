f09101b9fb9fba3a19fc722291d0f7c66c5d8e5a
雪花飘 <315680524@qq.com>
2022-04-07 17:33:18 +0800

    nothing

--- nlp/text_conversion/aspose_words.py
+++ nlp/text_conversion/aspose_words.py
@@ -6,7 +6,7 @@ def convert(file, suffix):
     """
     文件格式转换
     可以完成大多数格式之间的转换
-    
+
     :param file: 想要转换的文件
     :param suffix: 想要转换成的文件格式
     :return: 返回原始文件名的新文件格式文件
@@ -14,12 +14,14 @@ def convert(file, suffix):
     # 输入想输入的任何格式的文本
     input_file = aw.Document(file)
 
+    # 获取输入的文件文件名
     file_name = re.search('/?(.*?)\.', file).group(1)
 
     # 输出想要输出的格式文本，提供输出文件名及所要输入格式后缀
     output_file = input_file.save(file_name + '.' + suffix)
 
     return output_file
+
 
 if __name__ == "__main__":
     convert('resume_paper.pdf', 'txt')
