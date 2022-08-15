579845dc5cfce436f4e730f9e80b7792da89fd10
雪花飘 <315680524@qq.com>
2022-08-15 08:09:26 +0800

    update

--- nlp/nlp_tools/ocr/paddleocr_use.py
+++ nlp/nlp_tools/ocr/paddleocr_use.py
@@ -5,17 +5,19 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 from paddleocr import PaddleOCR, draw_ocr
 from utils import file_name
 from PIL import Image
+import time
 
 # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
 # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
-all_jpg = file_name('../imagepdf_to_image/image_1/')
+start_time = time.time()
+all_jpg = file_name('../imagepdf_to_image/image_2/')
 txt_content = ''
 for single_jpg in all_jpg:
-    img_path = f'../imagepdf_to_image/image_1/{single_jpg}'
+    img_path = f'../imagepdf_to_image/image_2/{single_jpg}'
     result = ocr.ocr(img_path, cls=True)
 
-    # print(result[0])
+    # print(result)
     # print(result[0][0])
     # print(result[0][0][0])
     # for line in result:
@@ -37,10 +39,12 @@ for single_jpg in all_jpg:
     # scores = [line[1][1] for line in result]
     # im_show = draw_ocr(image_1, boxes, txts, scores, font_path='./fonts/simfang.ttf')
     # im_show = Image.fromarray(im_show)
-    # im_show.save(f'124pdf_{single_jpg}')
+    # im_show.save(f'./result/result_{single_jpg}')
 
 
 print(txt_content)
+end_time = time.time()
+print(end_time-start_time)
 
 # 显示结果
 # from PIL import Image
