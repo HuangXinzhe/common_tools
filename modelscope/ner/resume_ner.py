cfa88a585c1933f3448caef318f4b67d263601ee
hxz <315680524@qq.com>
2022-08-16 08:13:35 +0800

    modelscope resume net

--- modelscope/ner/resume_ner.py
+++ modelscope/ner/resume_ner.py
@@ -1,7 +1,12 @@
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
+import time
 
 ner_pipeline = pipeline(Tasks.named_entity_recognition, 'damo/nlp_raner_named-entity-recognition_chinese-base-resume')
-result = ner_pipeline('常建良，男，')
+
+start_time = time.time()
+result = ner_pipeline('百度在线网络技术（北京）有限公司 常建良 男 java工程师 百度 java')
+end_time = time.time()
 
 print(result)
+print(end_time-start_time)
