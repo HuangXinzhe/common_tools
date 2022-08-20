2d18be99e17099e9314d9fa648b35488d1515040
HuangXinzhe <315680524@qq.com>
2022-08-20 12:53:51 +0800

    update

--- nlp/nlp_tools/ocr/cnocr_use.py
+++ nlp/nlp_tools/ocr/cnocr_use.py
@@ -4,35 +4,15 @@ import time
 
 # 简单用法
 # img_fp = r'D:\common_tools\common_tools\nlp\nlp_tools\imagepdf_to_image\image_1\101_0.jpg'
-img_fp = '/Users/huangxinzhe/Desktop/3641660748119_.pic.jpg'
+# img_fp = '/Users/huangxinzhe/Desktop/3641660748119_.pic.jpg'
 
 # ocr = CnOcr()  # 所有参数都使用默认值
-ocr = CnOcr(det_model_name='en_PP-OCRv3_det', rec_model_name='en_PP-OCRv3')  # 英文识别
+# ocr = CnOcr(det_model_name='en_PP-OCRv3_det', rec_model_name='en_PP-OCRv3')  # 英文识别
 
 
-out = ocr.ocr(img_fp)
-txt_content = ''
-for line_number in range(len(out)):
-        if line_number == 0:
-            txt_content += out[line_number]['text']
-        else:
-            if abs(out[line_number - 1]['position'][0][1] - out[line_number]['position'][0][1]) <= 15:
-                txt_content += ' ' + out[line_number]['text']
-            else:
-                txt_content += '\n' + out[line_number]['text']
-print(txt_content)
-
-
-# start_time = time.time()
-# all_jpg = file_name('../imagepdf_to_image/image_1/')
+# out = ocr.ocr(img_fp)
 # txt_content = ''
-# for single_jpg in all_jpg:
-#     img_fp = f'../imagepdf_to_image/image_1/{single_jpg}'
-#     ocr = CnOcr()  # 所有参数都使用默认值
-#     # ocr = CnOcr(rec_model_name='ch_PP-OCRv3')  # 所有参数都使用默认值
-#     out = ocr.ocr(img_fp)
-#     print(out)
-#     for line_number in range(len(out)):
+# for line_number in range(len(out)):
 #         if line_number == 0:
 #             txt_content += out[line_number]['text']
 #         else:
@@ -40,7 +20,27 @@ print(txt_content)
 #                 txt_content += ' ' + out[line_number]['text']
 #             else:
 #                 txt_content += '\n' + out[line_number]['text']
-#     txt_content += '\n'
 # print(txt_content)
-# end_time = time.time()
-# print(end_time-start_time)
+
+
+start_time = time.time()
+all_jpg = file_name('../imagepdf_to_image/image_1/')
+txt_content = ''
+for single_jpg in all_jpg:
+    img_fp = f'../imagepdf_to_image/image_1/{single_jpg}'
+    ocr = CnOcr()  # 所有参数都使用默认值
+    # ocr = CnOcr(rec_model_name='ch_PP-OCRv3')  # 所有参数都使用默认值
+    out = ocr.ocr(img_fp)
+    print(out)
+    for line_number in range(len(out)):
+        if line_number == 0:
+            txt_content += out[line_number]['text']
+        else:
+            if abs(out[line_number - 1]['position'][0][1] - out[line_number]['position'][0][1]) <= 15:
+                txt_content += ' ' + out[line_number]['text']
+            else:
+                txt_content += '\n' + out[line_number]['text']
+    txt_content += '\n'
+print(txt_content)
+end_time = time.time()
+print(end_time-start_time)
